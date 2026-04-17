ae741d5dc8c7161375985112eb035d4eff57dfea
davsla12 <122469382+davsla12@users.noreply.github.com>
2026-04-17 18:52:48 +0200

    Documentation

--- MF/Random.h
+++ MF/Random.h
@@ -1,7 +1,11 @@
 #ifndef RANDOM_H
 #define RANDOM_H
 
+//gives random between min and max
+//You should check min < max
 int randomInt(int min, int max);
+
+//Initialized random generator
 int init_random();
 
 #endif
