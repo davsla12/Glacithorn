797f2224b5811b306b72d38615623e49b4ce7efe
davsla12 <122469382+davsla12@users.noreply.github.com>
2026-04-17 19:01:57 +0200

    documentation

--- MF/screen.h
+++ MF/screen.h
@@ -10,23 +10,37 @@ struct Scane{
     WINDOW* window;
     int rows,collums; //define max size of the window
     int pos_y,pos_x;
-    int timer; //how often call main
+    int timer; //how often call main TODO
     std::list<sprite*> sprites;
     void (*input)(int key); //waits for input from user
     int (*main)();
     void (*render)(); //rendering somethink outside sprites
-    int ID = 0;
+    int ID = -1;
 };
 
 extern int screen_y;
 extern int screen_x;
 
+//gives size of scane
+//id 0 is main screen
 int GetSizey(int id);
 int GetSizex(int id);
+
+//add`s scane into scane`s stack
+//TODO: RmScane
+//\returns scane ID
 int AddScane(Scane* add);
+
+//initialized ncurses and sets COLOR_PAIR`s
 int screen_init();
+
+//ends ncurses
 void screen_delete();
+
+//Create ncurses window
 void CreateWin(Scane* scane,int rows,int collums,int y,int x);
+
+//Functions to call Scanes function
 int SInput();
 int SMain();
 int screen_update();
