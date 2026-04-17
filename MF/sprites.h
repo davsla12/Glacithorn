466c0a961b31a52b6906e8c67af618a5b0977c53
davsla12 <122469382+davsla12@users.noreply.github.com>
2026-04-17 19:04:03 +0200

    documentation

--- MF/sprites.h
+++ MF/sprites.h
@@ -2,10 +2,11 @@
 #define SPRITES_H
 #include <iostream>
 
+//simple struct to represent Sprite
 struct Sprite {
     int y = 2;
     int x = 7;
-    int color_pair = 0;
+    int color_pair = 0;//color pair inicialized in screen_init
     int high = 1;
     int width = 2;
     std::string mask = "{}";
